fd429b6eacef1d44d217528af89bd87ce862e647
Bernice Templeman <bernice.templeman@gmail.com>
2021-11-27 18:37:39 -0500

    Add files via upload

---
+++ typescript/hello.js
@@ -0,0 +1,7 @@
+var message = "Hello TS";
+console.log(message);
+function sayHello(name) {
+    return 'Hello ' + name;
+}
+var username = 'person';
+document.body.innerHTML = sayHello(username);
---
+++ typescript/hello.ts
@@ -0,0 +1,14 @@
+let message:string="Hello TS";
+console.log(message);
+
+function sayHello(name:string){
+    return 'Hello ' + name;
+
+}
+
+var username = 'person';
+
+document.body.innerHTML = sayHello(username);
+
+
+    
